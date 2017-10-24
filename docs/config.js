ef38f80855ffb1dcb1f5ae07c306210a730a274c
耐小心 <qiqizjl@qq.com>
2017-10-24 23:15:00 +0800

    add docs

---
+++ docs/config.js
@@ -0,0 +1,12 @@
+docute.init({
+    title: 'Go-Payment 支付微服务文档',
+    home: 'Home.md',
+    repo: 'qiqizjl/payment',
+    nav: [
+        {
+            title: '首页', path: '/'
+        },
+    ],
+    tocVisibleDepth: 2,
+    plugins: []
+});
